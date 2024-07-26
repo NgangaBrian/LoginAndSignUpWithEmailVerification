8a196cdd06989612a1d3646cc8a2b50262f19f23
NgangaBrian <ngangabrian724@gmail.com>
2024-07-26 16:43:30 +0300

    First Commit

--- app/src/main/java/com/example/loginandsignupwithemailverification/SignUp.java
+++ app/src/main/java/com/example/loginandsignupwithemailverification/SignUp.java
@@ -1,18 +1,26 @@
 package com.example.loginandsignupwithemailverification;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
     public TextView loginTV;
+    public Button registerBTN;
+    public EditText fullName, email, password, confirmPword;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        registerBTN = findViewById(R.id.registerBtn);
 
         loginTV = findViewById(R.id.toLogin);
         loginTV.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +31,7 @@ public class SignUp extends AppCompatActivity {
                 finish();
             }
         });
+
+
     }
 }
