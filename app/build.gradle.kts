851f1453c8755162dc9f7264dcdc1e8e6b506671
NgangaBrian <ngangabrian724@gmail.com>
2024-07-27 22:18:39 +0300

    First Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,11 +20,26 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+
+            buildConfigField ("String", "SENDER_EMAIL", "\"${project.findProperty("SENDER_EMAIL".toString()) ?: "default@example.com"}\"")
+            buildConfigField ("String", "EMAIL_PASS", "\"${project.findProperty("SENDER_EMAIL_PASSWORD".toString()) ?: "defaultPassword"}\"")
+    }
+        debug {
+            buildConfigField ("String", "SENDER_EMAIL", "\"${project.findProperty("SENDER_EMAIL".toString()) ?: "default@example.com"}\"")
+            buildConfigField ("String", "EMAIL_PASS", "\"${project.findProperty("SENDER_EMAIL_PASSWORD".toString()) ?: "defaultPassword"}\"")
         }
     }
+    buildFeatures {
+        buildConfig = true // Enable BuildConfig feature
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    packagingOptions{
+        exclude("META-INF/NOTICE.md")
+        exclude("META-INF/LICENSE.md")
     }
 }
 
@@ -34,6 +49,8 @@ dependencies {
     implementation(libs.material)
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation("com.sun.mail:android-mail:1.6.6")
+    implementation("com.sun.mail:android-activation:1.6.7")
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
--- app/src/main/java/com/example/loginandsignupwithemailverification/SignUp.java
+++ app/src/main/java/com/example/loginandsignupwithemailverification/SignUp.java
@@ -7,20 +7,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.Properties;
+import java.util.Random;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 
 public class SignUp extends AppCompatActivity {
     public TextView loginTV;
     public Button registerBTN;
-    public EditText fullName, email, password, confirmPword;
+    public EditText fullName, userName, uEmail, pWord, confirmPword;
+    public String verificationCode;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        fullName = findViewById(R.id.fullname);
+        userName = findViewById(R.id.username);
+        uEmail = findViewById(R.id.email);
+        pWord = findViewById(R.id.password);
+        confirmPword = findViewById(R.id.confirmPassword);
         registerBTN = findViewById(R.id.registerBtn);
+
+
+
 
         loginTV = findViewById(R.id.toLogin);
         loginTV.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +57,82 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
+        registerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storeDetails();
+                generateVerificationCode();
+                Toast.makeText(SignUp.this, verificationCode, Toast.LENGTH_LONG).show();
+            }
+        });
 
+
+    }
+
+    private void storeDetails() {
+        String fullname = fullName.getText().toString();
+        String username = userName.getText().toString();
+        String email = uEmail.getText().toString();
+        String password = pWord.getText().toString();
+        String confirmPass = confirmPword.getText().toString();
+
+    }
+
+    public void sendEmail(String stringReceiverEmail, String verificationCode){
+        try{
+        String stringSenderEmail = BuildConfig.SENDER_EMAIL;
+        String stringPassSenderEmail = BuildConfig.EMAIL_PASS;
+
+        String stringHost = "smtp.gmail.com";
+
+        Properties properties = System.getProperties();
+
+        properties.put("mail.smtp.host", stringHost);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        javax.mail.Session session = Session.getInstance(properties, new Authenticator(){
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return  new PasswordAuthentication(stringSenderEmail, stringPassSenderEmail);
+            }
+        });
+
+        MimeMessage mimeMessage = new MimeMessage(session);
+        mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(stringReceiverEmail));
+
+        String emailContent = String.format("Hello Brian, \n\nYour verification code is %s \n\n Regards, Brian", verificationCode);
+        mimeMessage.setSubject("Subject: Verification Code");
+        mimeMessage.setText(emailContent);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Transport.send(mimeMessage);
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
+
+        thread.start();
+
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public void generateVerificationCode(){
+        Random random = new Random();
+        int code = 100000 + random.nextInt(900000);
+        verificationCode = String.valueOf(code);
+        String userEmail = uEmail.getText().toString();
+        sendEmail(userEmail, verificationCode);
     }
 }
